29f89a38a5118fde291bc481c31ef265be3d7a48
DAle <Dalepantano54@gmail.com>
2025-02-08 09:29:19 +0800

    dale
    211233232ssafsf

---
+++ scripts.js
@@ -0,0 +1,11 @@
+
+function greetUser() {
+    alert("Welcome to my website!");
+}
+
+function showHobbies() {
+    alert("My hobbies are: Reading, Playing Mobile Games, Traveling, and Coding!");
+}
+
+
+window.onload = greetUser;
